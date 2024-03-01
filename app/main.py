5f211ce12ca4a71e7a6b95866a5378d5626a692a
letsticia <leti.morais48@gmail.com>
2024-03-01 12:16:25 -0300

    refactor: added columns

--- app/main.py
+++ app/main.py
@@ -1,1 +1,30 @@
 import streamlit as st
+
+st.set_page_config(
+    page_title="Surface Crack Detection",
+    page_icon="🧱",
+    layout="wide", 
+)
+
+st.header("Surface Crack Detection AI App")
+
+
+left_column, right_column = st.columns(2)
+
+with left_column:
+    st.header("Source Image")
+    source = st.radio("Get Image from", ["Camera", "Upload"])
+    
+    if source == "Camera":
+        source_img = st.camera_input("Take a picture from your camera")
+    elif source == "Upload":
+        source_img = st.file_uploader("Upload an image here")
+
+with right_column:
+    st.header("Result Image")
+    
+    if source_img is not None:
+        st.image(source_img, use_column_width=True)
+        st.table({"Prediction": ["Crack", "No Crack"], "Confidence": ["0.8", "0.2"]}) # examples only
+    else:
+        st.info("Please select an image from the left column")
